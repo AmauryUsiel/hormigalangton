9cd88bae1bec7bd58d6997f1cf9b9fb860d74ff3
AmauryUsiel <darksouls90dmc@gmail.com>
2021-02-05 16:59:46 -0600

    Ajustes a Matriz para lectura de archivos

--- hormiga_langton/matriz.py
+++ hormiga_langton/matriz.py
@@ -1,10 +1,16 @@
 class Matriz:
-    def __init__(self, ancho, altura):
-        self.ancho = ancho
-        self.altura = altura
-        self.matriz = [
-            [0 for columnas in range(0, ancho)] for filas in range(0, altura)
-        ]
+    def __init__(self, ancho, altura, matriz=None):
+
+        if matriz is None:
+            self.ancho = ancho
+            self.altura = altura
+            self.matriz = [
+                [0 for columnas in range(0, ancho)] for filas in range(0, altura)
+            ]
+        else:
+            self.ancho = len(matriz[0])
+            self.altura = len(matriz)
+            self.matriz = matriz
 
     def obt_color_casilla(self, posicion):
         x, y = posicion
