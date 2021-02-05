42ffd79e61d4b9fe4480c8a2cec8b20e0531771c
AmauryUsiel <darksouls90dmc@gmail.com>
2021-02-05 17:16:47 -0600

    Cambio en la interfaz

--- interfaz/ventana.py
+++ interfaz/ventana.py
@@ -8,17 +8,21 @@ class Ventana:
         self.root.geometry(tam)
 
         # crear el layout
-        visor = tk.Frame(master=self.root, width=400, height=1000, bg="red")
+        visor = tk.Frame(master=self.root, width=400, height=1000, bg="gray")
         visor.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-        controles = tk.Frame(master=self.root, width=300, height=1000, bg="blue")
+        controles = tk.Frame(master=self.root, width=300, height=1000, bg="gray")
         controles.pack(fill=tk.BOTH, side=tk.LEFT, expand=False)
-        controles.pack_propagate(0)
 
         self.hacer_controles(controles)
 
-    def hacer_controles(self, contenedor):
-        label = tk.Label(master=contenedor, text="Panel de control")
-        label.pack()
+    def hacer_controles(self, controles):
+
+        contenedor = tk.Frame(master=controles, width=300, height=1000, bg="blue")
+        contenedor.pack(fill=tk.BOTH, side=tk.LEFT, expand=False, padx=20, pady=20)
+        contenedor.pack_propagate(0)
+
+        label = tk.Label(master=contenedor, text="Elegir variante")
+        label.pack(side=tk.TOP, anchor=tk.NW)
 
     def ejecutar(self):
         self.root.mainloop()
